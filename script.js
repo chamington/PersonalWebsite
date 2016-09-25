a2af9c9aa919093dade44c592a686243081ffcf3
chamington <cosmictartar@gmail.com>
2016-09-24 20:29:51 -0400

    Some animations

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ window.onscroll = function (e) {
     else if(pageYOffset==0){
         window.scrollTo(0,document.body.scrollHeight);
     }
+    document.getElementById('image').style.marginLeft = (document.getElementById("name").clientHeight *2)-pageYOffset;
 }
