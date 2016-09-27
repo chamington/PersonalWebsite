557e5879302a88382abb317a9160b1f140a20486
chamington <cosmictartar@gmail.com>
2016-09-26 22:50:49 -0400

    Oh yeah, so much better
    Yes Mark, I do know what a loop and an array is

--- colors.js
+++ colors.js
@@ -1,95 +1,17 @@
-//look, I know I should make loops with arrays for this, but this is just a temporary (I hope lol) fix
-//I am so sorry for writing the code to be this bad, I'kll try to remember to fix it later
+//There, better
+var c = [,,,,]
+for (b=0; b <= 5; b++){
+    c[b]="rgb("+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+")";
+}
 
-var c1="rgb("+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+")";
-var c2="rgb("+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+")";
-var c3="rgb("+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+")";
-var c4="rgb("+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+")";
-var c5="rgb("+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+","+(Math.floor(Math.random() * 55) + 200)+")";
-
-document.getElementById("name").style.backgroundImage =         "linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("succeed").style.backgroundImage =      "linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("math").style.backgroundImage =         "linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("excel").style.backgroundImage =        "linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("resume").style.backgroundImage =       "linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("nameagain").style.backgroundImage =    "linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("trump").style.backgroundImage =        "linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("redorgreen").style.backgroundImage =   "linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("gamble").style.backgroundImage =       "linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("champernowne").style.backgroundImage = "linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("shakespere").style.backgroundImage =   "linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("trumpagain").style.backgroundImage =   "linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("MKA").style.backgroundImage =          "linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("GPA").style.backgroundImage =          "linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("MLC").style.backgroundImage =          "linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("TALK").style.backgroundImage =         "linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("NASA").style.backgroundImage =         "linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("MKAagain").style.backgroundImage =     "linear-gradient(" + c5 + ", " + c1 + ")";
-
-
-document.getElementById("name").style.backgroundImage =         "-moz-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("succeed").style.backgroundImage =      "-moz-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("math").style.backgroundImage =         "-moz-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("excel").style.backgroundImage =        "-moz-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("resume").style.backgroundImage =       "-moz-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("nameagain").style.backgroundImage =    "-moz-linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("trump").style.backgroundImage =        "-moz-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("redorgreen").style.backgroundImage =   "-moz-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("gamble").style.backgroundImage =       "-moz-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("champernowne").style.backgroundImage = "-moz-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("shakespere").style.backgroundImage =   "-moz-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("trumpagain").style.backgroundImage =   "-moz-linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("MKA").style.backgroundImage =          "-moz-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("GPA").style.backgroundImage =          "-moz-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("MLC").style.backgroundImage =          "-moz-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("TALK").style.backgroundImage =         "-moz-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("NASA").style.backgroundImage =         "-moz-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("MKAagain").style.backgroundImage =     "-moz-linear-gradient(" + c5 + ", " + c1 + ")";
-
-
-document.getElementById("name").style.backgroundImage =         "-webkit-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("succeed").style.backgroundImage =      "-webkit-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("math").style.backgroundImage =         "-webkit-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("excel").style.backgroundImage =        "-webkit-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("resume").style.backgroundImage =       "-webkit-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("nameagain").style.backgroundImage =    "-webkit-linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("trump").style.backgroundImage =        "-webkit-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("redorgreen").style.backgroundImage =   "-webkit-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("gamble").style.backgroundImage =       "-webkit-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("champernowne").style.backgroundImage = "-webkit-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("shakespere").style.backgroundImage =   "-webkit-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("trumpagain").style.backgroundImage =   "-webkit-linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("MKA").style.backgroundImage =          "-webkit-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("GPA").style.backgroundImage =          "-webkit-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("MLC").style.backgroundImage =          "-webkit-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("TALK").style.backgroundImage =         "-webkit-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("NASA").style.backgroundImage =         "-webkit-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("MKAagain").style.backgroundImage =     "-webkit-linear-gradient(" + c5 + ", " + c1 + ")";
-
-
-document.getElementById("name").style.backgroundImage =         "-o-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("succeed").style.backgroundImage =      "-o-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("math").style.backgroundImage =         "-o-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("excel").style.backgroundImage =        "-o-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("resume").style.backgroundImage =       "-o-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("nameagain").style.backgroundImage =    "-o-linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("trump").style.backgroundImage =        "-o-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("redorgreen").style.backgroundImage =   "-o-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("gamble").style.backgroundImage =       "-o-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("champernowne").style.backgroundImage = "-o-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("shakespere").style.backgroundImage =   "-o-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("trumpagain").style.backgroundImage =   "-o-linear-gradient(" + c5 + ", " + c1 + ")";
-
-document.getElementById("MKA").style.backgroundImage =          "-o-linear-gradient(" + c5 + ", " + c1 + ")";
-document.getElementById("GPA").style.backgroundImage =          "-o-linear-gradient(" + c1 + ", " + c2 + ")";
-document.getElementById("MLC").style.backgroundImage =          "-o-linear-gradient(" + c2 + ", " + c3 + ")";
-document.getElementById("TALK").style.backgroundImage =         "-o-linear-gradient(" + c3 + ", " + c4 + ")";
-document.getElementById("NASA").style.backgroundImage =         "-o-linear-gradient(" + c4 + ", " + c5 + ")";
-document.getElementById("MKAagain").style.backgroundImage =     "-o-linear-gradient(" + c5 + ", " + c1 + ")";
+var links1 = ["name","succeed","math","excel","resume","nameagain"]
+var links2 = ["trump","redorgreen","gamble","champernowne","shakespere","trumpagain"]
+var links3 = ["MKA","GPA","MLC","TALK","NASA","MKAagain"]
+var suffexes = ["","-moz-","-webkit-","-o-"] //I fucking hate how 
+for (a = 0; a < suffexes.length; a++) {
+    for (ia=0; ia<=5; ia++){
+        document.getElementById(links1[ia%6]).style.backgroundImage = suffexes[a]+"linear-gradient(" + c[ia%5] + ", " + c[(ia+1)%5] + ")";
+        document.getElementById(links2[ia%6]).style.backgroundImage = suffexes[a]+"linear-gradient(" + c[ia%5] + ", " + c[(ia+1)%5] + ")";
+        document.getElementById(links3[ia%6]).style.backgroundImage = suffexes[a]+"linear-gradient(" + c[ia%5] + ", " + c[(ia+1)%5] + ")";
+    }
+}
